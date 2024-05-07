c556703af7c7c4234bc0ea9c80c415b7a5ce821b
Code-Gale <codewhiz001@gmail.com>
2024-05-07 02:02:28 +0100

    routes

---
+++ backend/src/routes/userRoutes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router()
+const { getAll, getById, updateUser, deleteUser } = require('../controllers/userController')
+
+router.get('/', getAll)
+router.get('/:id', getById)
+router.put('/:id', updateUser)
+router.delete('/:id', deleteUser)
+
+module.exports = router;
