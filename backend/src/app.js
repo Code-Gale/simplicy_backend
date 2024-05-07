da667f24843b4b20f92dfb59574a790491d5922b
Code-Gale <codewhiz001@gmail.com>
2024-05-07 02:05:24 +0100

    added users routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
 const authRoutes = require('./routes/authRoutes')
+const userRoutes = require('./routes/userRoutes')
 
 // loading env variables
 dotenv.config()
@@ -16,6 +17,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended : true}))
 
 app.use('/api/auth', authRoutes)
+app.use('/api/users/', userRoutes)
 
 const PORT = process.env.PORT || 3000;
 
