409ce1949f53be5a6419a481111c86a54c4bfed3
Code-Gale <codewhiz001@gmail.com>
2024-05-07 01:23:41 +0100

    updated app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+const authRoutes = require('./routes/authRoutes')
 
 // loading env variables
 dotenv.config()
@@ -7,11 +9,16 @@ dotenv.config()
 //starting app
 const app = express()
 
+connectDB()
+
 //middlewares
 app.use(express.json())
 app.use(express.urlencoded({ extended : true}))
 
+app.use('/api/auth', authRoutes)
+
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log('Listening on Port', PORT)
 })
