abfca3a2c21660629c740a8ea597713b9f1c204d
Code-Gale <codewhiz001@gmail.com>
2024-05-14 18:14:40 +0100

    updated route for adding new products

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,13 +1,14 @@
 const router = require('express').Router()
-const { getAll, getById, addProduct, updateProduct, deleteProduct } = require('../controllers/productController')
+const { getAll, getById, getProductBySeller, addProduct, updateProduct, deleteProduct } = require('../controllers/productController')
 const authMiddleware = require('../middlewares/authMiddleware')
 
 // Routes accessible to both buyers and sellers
 router.get('/', getAll)
 router.get('/:id', getById)
+router.get('/:id/products', getProductBySeller)
 
 // Routes accessible to only sellers
-router.post('/', authMiddleware, addProduct)
+router.post('/newProduct', authMiddleware, addProduct)
 router.put('/:id', authMiddleware, updateProduct)
 router.delete('/:id', authMiddleware, deleteProduct)
 
